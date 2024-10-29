39d2baa5cd6ca613fb161cba85fbc3b1249be59a
goujon.n@gmail.com <goujon.n@gmail.com>
2024-10-29 15:51:04 +0000

    vdef

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,6 +2,10 @@ select
 orders_id
 ,date_date
 ,ROUND(SUM((margin + shipping_fee) - (logcost + ship_cost)),2) AS operational_margin
+,SUM(shipping_fee) AS shipping_fee
+,SUM(revenue) AS revenue
+,SUM(margin) AS margin
+,SUM(purchase_cost) AS purchase_cost
 ,sum(logcost) AS logcost
 ,sum(ship_cost) AS ship_cost
 FROM {{ ref('int_orders_margin') }}
