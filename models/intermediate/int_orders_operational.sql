040fb6b53db63aafdc30eaf4b5f26d02865b3f0f
goujon.n@gmail.com <goujon.n@gmail.com>
2024-10-29 13:59:40 +0000

    revenue, qty, purchase cost & margin grouped by orders

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,9 @@
+select
+orders_id
+,date_date
+,ROUND(SUM((margin + shipping_fee) - (logcost + ship_cost)),2) AS operational_margin
+FROM {{ ref('int_orders_margin') }}
+LEFT JOIN {{ ref('stg_raw__ship') }}
+USING (orders_id)
+GROUP BY orders_id, date_date
+ORDER BY orders_id DESC
