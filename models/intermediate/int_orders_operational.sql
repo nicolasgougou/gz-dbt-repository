a917784518671e3b8c8cb0cb84633c4f9d41892c
goujon.n@gmail.com <goujon.n@gmail.com>
2024-10-29 15:52:56 +0000

    defdef

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -8,6 +8,7 @@ orders_id
 ,SUM(purchase_cost) AS purchase_cost
 ,sum(logcost) AS logcost
 ,sum(ship_cost) AS ship_cost
+,SUM(quantity) AS quantity
 FROM {{ ref('int_orders_margin') }}
 LEFT JOIN {{ ref('stg_raw__ship') }}
 USING (orders_id)
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,18 +1,14 @@
 select
 int_orders_operational.date_date 
-,COUNT (int_orders_operational.orders_id) AS nb_transactions
+,COUNT (orders_id) AS nb_transactions
 ,ROUND(SUM (revenue)) AS total_revenue
-,ROUND(SUM(revenue) / COUNT(orders_id)) AS average_basket
+,ROUND (ROUND(SUM (revenue))/ COUNT (orders_id),2) AS average_basket
 ,ROUND(SUM (margin)) AS margin
 ,ROUND(SUM (operational_margin)) AS total_operational_margin
 ,ROUND(SUM (purchase_cost)) AS total_purchase_cost 
 ,ROUND(SUM (shipping_fee)) AS total_shipping_fee
-,ROUND(SUM (raw_ship.logcost)) AS total_logcost
+,ROUND(SUM (logcost)) AS total_logcost
 ,SUM (quantity) AS total_quantity_products_sold
-FROM {{ ref('int_orders_operational') }} AS int_orders_operational
-LEFT JOIN {{ ref('int_sales_margin') }}
-USING (orders_id)
-LEFT JOIN {{ ref('stg_raw__ship') }} AS raw_ship
-USING (orders_id)
+FROM {{ ref('int_orders_operational') }}
 GROUP BY date_date
 ORDER BY date_date DESC
