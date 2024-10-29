40c4ef0f80487bb1ec5472476bf59e18b4283b98
goujon.n@gmail.com <goujon.n@gmail.com>
2024-10-29 14:32:57 +0000

    operational margin grouped by order

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,6 +2,8 @@ select
 orders_id
 ,date_date
 ,ROUND(SUM((margin + shipping_fee) - (logcost + ship_cost)),2) AS operational_margin
+,sum(logcost) AS logcost
+,sum(ship_cost) AS ship_cost
 FROM {{ ref('int_orders_margin') }}
 LEFT JOIN {{ ref('stg_raw__ship') }}
 USING (orders_id)
