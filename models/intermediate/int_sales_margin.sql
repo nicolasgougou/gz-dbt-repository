89d00a32da8f0f90395376be1a95ee99ce01d7ff
goujon.n@gmail.com <goujon.n@gmail.com>
2024-10-30 14:05:17 +0000

    margin_percent

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -2,6 +2,7 @@ select
 *
 ,ROUND(quantity * purchase_price) AS purchase_cost
 ,ROUND(revenue - (quantity * purchase_price)) AS margin
+,{{ margin_percent('revenue', 'quantity * CAST(purchase_price AS FLOAT64)') }} as margin_percent
 FROM {{ ref('stg_raw__product') }}
 INNER JOIN {{ ref('stg_raw__sales') }}
 USING (products_id)
