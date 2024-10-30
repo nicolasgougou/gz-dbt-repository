e07923a8506d4362f3e5c043039532525ca18a8d
goujon.n@gmail.com <goujon.n@gmail.com>
2024-10-30 15:02:35 +0000

    d

--- snapshots/products_price.sql
+++ snapshots/products_price.sql
@@ -2,10 +2,10 @@
 
 {{
     config(
-      target_schema='dbt_snapshots', --corresponding to the new database in BigQuery
-      unique_key='products_id', --unique key of the table to track
-      strategy='check', --other strategy
-      check_cols='all' --track evolution on all columns
+      target_schema='dbt_snapshots',
+      unique_key='products_id',
+      strategy='check',
+      check_cols='all'
     )
 }}
 
